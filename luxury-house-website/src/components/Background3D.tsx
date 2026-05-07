4bf458bdea187e2b780b0479321ce2b5eb7e28a8
Khushal Yadav <khushalyadav535@gmail.com>
2026-05-07 18:41:32 +0530

    fix typescript build errors for 3d components

--- luxury-house-website/src/components/Background3D.tsx
+++ luxury-house-website/src/components/Background3D.tsx
@@ -42,15 +42,11 @@ function ParticleRing() {
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
-          count={particlesCount}
-          array={positions}
-          itemSize={3}
+          args={[positions, 3]}
         />
         <bufferAttribute
           attach="attributes-color"
-          count={particlesCount}
-          array={colors}
-          itemSize={3}
+          args={[colors, 3]}
         />
       </bufferGeometry>
       <pointsMaterial
--- luxury-house-website/src/components/VisualMastery3D.tsx
+++ luxury-house-website/src/components/VisualMastery3D.tsx
@@ -43,8 +43,6 @@ export default function VisualMastery3D() {
         
         <PresentationControls
           global
-          config={{ mass: 2, tension: 500 }}
-          snap={{ mass: 4, tension: 1500 }}
           rotation={[0, 0.3, 0]}
           polar={[-Math.PI / 3, Math.PI / 3]}
           azimuth={[-Math.PI / 1.4, Math.PI / 2]}
